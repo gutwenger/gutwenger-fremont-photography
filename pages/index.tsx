6b033952e45f8aa05f88c18d85e1f2380b2dc613
gutwenger <chengfremont@gmail.com>
2022-06-26 22:19:19 +0800

    Added hostname for cloudinary

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,14 @@ import type { NextPage } from 'next'
 import styles from '../styles/Home.module.scss'
 import Image from 'next/image';
 
-// Import IMG_0349 from assets/images
-import IMG_0349 from '../assets/images/IMG_0349.jpg'
-
-
 const Home: NextPage = () => {
+
+  const BACKGROUND_IMAGE_URL = 'https://res.cloudinary.com/fremont-cheng/image/upload/v1656252809/enol61pvtdmto1tmlwse.jpg';
+
   return (
     <div className={styles.home}>
       <div className={styles.homeBackground}>
-        <Image src={IMG_0349} alt="IMG_0349" layout="fill" sizes="100vw"/>
+        <Image src={BACKGROUND_IMAGE_URL} alt="background image" layout="fill" sizes="100vw"/>
       </div>
       <div className={styles.homeContent}>
         <h1>Fremont Cheng</h1>
